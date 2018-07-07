5bf0af4e58b650051d6c4cd7c144032b7143c3c7
marwan-at-work <marwan.sameer@gmail.com>
2018-07-07 17:21:00 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marwan-at-work/vgotest
+
+require github.com/kr/pretty v0.1.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kr/pretty"
+)
+
+func main() {
+	pretty.Println("yello")
+}
